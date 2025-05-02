6a48a28dfac0d84b7ea00242481c88fc6e9de852
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-05-02 18:05:59 -0300

    feat(Modal): inclui modal

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,63 @@
+import { useId } from "site/sdk/useId.ts";
+import { useSignal } from "@preact/signals";
+import { ComponentChildren } from "preact";
+import { useEffect } from "preact/hooks";
+
+interface Props {
+  onClose?: () => void;
+  open?: boolean;
+  class?: string;
+  style?: string;
+  children?: ComponentChildren;
+  loading?: "eager" | "lazy";
+}
+
+function Modal(props: Props) {
+  const {
+    children,
+    open,
+    onClose,
+    loading = "lazy",
+  } = props;
+  const lazy = useSignal(loading === "lazy" && !open);
+  const id = useId();
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) =>
+      (e.key === "Escape" || e.keyCode === 27) && open && onClose?.();
+
+    addEventListener("keydown", handler);
+
+    return () => {
+      removeEventListener("keydown", handler);
+    };
+  }, [open]);
+
+  useEffect(() => {
+    lazy.value = false;
+  }, []);
+
+  return (
+    <>
+      <input
+        id={id}
+        checked={open}
+        type="checkbox"
+        class="modal-toggle"
+        onChange={(e) => e.currentTarget.checked === false && onClose?.()}
+      />
+      <div class="modal flex items-center justify-center">
+        {!lazy.value && children}
+        <label
+          class="modal-backdrop absolute w-full h-full"
+          style={{ zIndex: 1 }}
+          for={id}
+        >
+          Close
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default Modal;
